dfe681bb1184414426090500d53e4694cfcd44ca
kobe081024 <105697349+kobe081024@users.noreply.github.com>
2025-01-14 12:40:10 +0900

    Update ライト(cのみ残す).user.js

--- ライト(cのみ残す).user.js
+++ ライト(cのみ残す).user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name		busyodasライトでcだけ残す
+// @name		ライトでcだけ残す
 // @namespace	https://gist.github.com/RAPT21/
 // @description	ブラウザ三国志 ブショーダス補助 by RAPT kobe編集
 // @include		https://*.3gokushi.jp/busyodas/busyodas_continuty_result.php*
